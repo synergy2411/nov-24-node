93abdfcf0bdb2b3191255de2e3dd2370223e4c66
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-30 11:33:39 +0530

    module system added

--- 01-basics/module-system-demo.js
+++ 01-basics/module-system-demo.js
@@ -1,5 +1,11 @@
 const { sum, getMyLuckyNumber } = require("./utils/maths");
+require("colors");
+const yargs = require("yargs");
 
 console.log("Sum : ", sum(2, 4));
 
 console.log("Lucky Number : ", getMyLuckyNumber());
+
+console.log("Success Message".green);
+
+console.log(yargs.argv);
