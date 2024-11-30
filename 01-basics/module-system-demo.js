ea3bbc441f125e5252941ceb79bbe4a2f2c66132
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-30 11:54:54 +0530

    core modules added

--- 01-basics/module-system-demo.js
+++ 01-basics/module-system-demo.js
@@ -1,11 +1,44 @@
+// File Module
 const { sum, getMyLuckyNumber } = require("./utils/maths");
+
+// External Module
 require("colors");
 const yargs = require("yargs");
 
-console.log("Sum : ", sum(2, 4));
+// Core Module
+const { arch, cpus, totalmem, freemem } = require("os");
 
-console.log("Lucky Number : ", getMyLuckyNumber());
+const { basename, extname, dirname } = require("path");
 
-console.log("Success Message".green);
+const { createServer } = require("http");
 
-console.log(yargs.argv);
+const server = createServer((request, response) => {
+  response.write(JSON.stringify({ message: "Hello World" }));
+  response.end();
+});
+
+server.listen(9000, () => console.log("Server started at PORT : 9000"));
+
+// let urlPath = "http://www.domain.com/public/index.html";
+
+// console.log(basename(urlPath));
+// console.log(extname(urlPath));
+// console.log(dirname(urlPath));
+
+// console.log("Architecture : ", arch());
+
+// console.log("Number of CPU's : ", cpus().length);
+
+// console.log("Total Memory : ", totalmem());
+
+// console.log("Free Memory : ", freemem());
+
+// console.log("Sum : ", sum(2, 4));
+
+// console.log("Lucky Number : ", getMyLuckyNumber());
+
+// console.log("Success Message".green);
+
+// console.log(yargs.argv);
+
+// node filename.js --title="the node app"
