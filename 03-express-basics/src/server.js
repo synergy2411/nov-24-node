d6152cc70dd3aa5d4617af7bf52f153616c318eb
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-30 16:21:06 +0530

    patch added

--- 03-express-basics/src/server.js
+++ 03-express-basics/src/server.js
@@ -36,6 +36,35 @@ app.get("/api/todos/:todoId", (req, res) => {
   }
 });
 
+app.delete("/api/todos/:todoId", (req, res) => {
+  const { todoId } = req.params;
+  const position = todoCollection.findIndex((todo) => todo.id === todoId);
+  if (position === -1) {
+    return res.sendStatus(400);
+  } else {
+    const [deletedItem] = todoCollection.splice(position, 1);
+    return res.send(deletedItem);
+    // return res.sendStatus(204)
+  }
+});
+
+app.patch("/api/todos/:todoId", (req, res) => {
+  const { todoId } = req.params;
+  const { title, status } = req.body;
+  const position = todoCollection.findIndex((todo) => todo.id === todoId);
+  if (position === -1) {
+    return res.sendStatus(400);
+  } else {
+    if (typeof title === "string") {
+      todoCollection[position].title = title;
+    }
+    if (typeof status === "boolean") {
+      todoCollection[position].status = status;
+    }
+    return res.send({ message: "updated" });
+  }
+});
+
 // app.get("/api", function (req, res) {
 //   res.send({ message: "Hello Express!!" });
 //   res.sendFile(path.join(__dirname, "/public/index.html"));
