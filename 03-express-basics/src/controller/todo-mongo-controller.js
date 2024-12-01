3498a42c97e2c14932553fd01d47855beda10501
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-01 12:09:26 +0530

    todo created

--- 03-express-basics/src/controller/todo-mongo-controller.js
+++ 03-express-basics/src/controller/todo-mongo-controller.js
@@ -9,4 +9,15 @@ const getAllTodos = async (req, res) => {
   }
 };
 
-module.exports = { getAllTodos };
+const createTodo = async (req, res) => {
+  try {
+    const newTodo = new TodoModel(req.body);
+    const createdTodo = await newTodo.save();
+    console.log(createdTodo);
+    res.sendStatus(201);
+  } catch (err) {
+    return res.send(err);
+  }
+};
+
+module.exports = { getAllTodos, createTodo };
--- 03-express-basics/src/router/todo-routes.js
+++ 03-express-basics/src/router/todo-routes.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const {
+  getAllTodos,
   getSingleTodo,
   createTodo,
   updateTodo,
   deleteTodo,
 } = require("../controller/todo-controller");
 
-const { getAllTodos } = require("../controller/todo-mongo-controller");
+const todoMongoController = require("../controller/todo-mongo-controller");
 
 const todoRouter = express.Router();
 
-todoRouter.route("/").get(getAllTodos).post(createTodo);
+todoRouter
+  .route("/")
+  .get(todoMongoController.getAllTodos)
+  .post(todoMongoController.createTodo);
 todoRouter
   .route("/:todoId")
   .get(getSingleTodo)
