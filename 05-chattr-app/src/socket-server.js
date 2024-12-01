793a63489effaf256ec26acedf73900018089887
Sumit Khandelwal <synergy2411@outlook.com>
2024-12-01 15:26:24 +0530

    chattr app

--- 05-chattr-app/src/socket-server.js
+++ 05-chattr-app/src/socket-server.js
@@ -10,8 +10,14 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "/public/socket-client.html"));
 });
 
-io.on("connection", () => {
+io.on("connection", (client) => {
   console.log("Client connected...");
+  client.emit("acknowledge", { message: "You are connected now!" });
+  client.on("message", (data) => {
+    console.log(data.chatterName + " says - ", data.message);
+    client.emit("reply", { chatterName: "Me", message: data.message });
+    client.broadcast.emit("reply", data);
+  });
 });
 
 server.listen(9010, () => console.log("Socket server running on PORT: 9010"));
